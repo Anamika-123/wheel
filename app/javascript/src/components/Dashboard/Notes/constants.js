52a273511484b0fd52909af28c4b2f6f26886e86
Anamika Sharma <anamika.sharma@bigbinary.com>
2021-12-30 23:00:46 +0530

    Created note listing page

---
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const CARD_DATA = [
+  {
+    id: 1,
+    title: "How to claim the warranty?",
+    description: "My laptop got shut down suddently. How can I claim the warranty online?",
+    tags: ["laptop"],
+    createdAt: { date: "Wednesday 10.30 am", timeAgo: "2 hours ago" },
+  },
+  {
+    id: 2,
+    title: "How to claim the warranty?",
+    description: "My laptop got shut down suddently. How can I claim the warranty online?",
+    tags: ["laptop"],
+    createdAt: { date: "Thursday 12.30 am", timeAgo: "3 hours ago" }
+  },
+  {
+    id: 3,
+    title: "How to claim the warranty?",
+    description: "My laptop got shut down suddently. How can I claim the warranty online?",
+    tags: ["laptop"],
+    createdAt: { date: "Monday 10.30 am", timeAgo: "4 days ago" }
+  },
+  {
+    id: 4,
+    title: "How to claim the warranty?",
+    description: "My laptop got shut down suddently. How can I claim the warranty online?",
+    tags: ["laptop"],
+    createdAt: { date: "Friday 12.30 pm", timeAgo: "4 days ago" }
+  }
+];
